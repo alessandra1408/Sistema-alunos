27b5a6205ac1850faabbc288d7c3c90306ea01f1
alessandra1408 <alessandraoitnb@gmail.com>
2021-08-25 19:16:06 -0300

    Cria a pagina principal

---
+++ src/pages/Homepage/index.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Route from '../../route'
+import { Link } from 'react-router-dom';
+import Registerpage from '../Registerpage';
+
+class Homepage extends Component{
+    render(){
+        return(
+            <>
+            <h1>Home page</h1>
+            </>
+            
+        )
+    }
+}
+
+export default Homepage;
