01230a998ac241a4bb0c7e0eac9ae9f0375767aa
alessandra1408 <alessandraoitnb@gmail.com>
2021-08-25 19:18:44 -0300

    Cria a pagina de registro

---
+++ src/pages/Registerpage/index.jsx
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import Header from '../../components/Header'
+import FormRegister from '../../components/FormRegister';
+import Homepage from '../../pages/Homepage'
+import Editpage from '../Editpage';
+import Route from '../../route';
+import { Link } from 'react-router-dom';
+
+class Registerpage extends Component {
+    render() {
+        const formNewStudent = [
+            {
+                labelName: 'Nome do Aluno',
+                inputId: 'studentName'
+            },
+            {
+                labelName: 'Data de Nascimento',
+                inputId: 'dayBirth'
+            },
+            {
+                labelName: 'Responsável pelo Aluno',
+                inputId: 'studentGuardian'
+            },
+            {
+                labelName: 'Número do Responsável pelo Aluno',
+                inputId: 'guardianNumber'
+            },
+            {
+                labelName: 'Em caso de Emergência avisar',
+                inputId: 'emergencyCase'
+            },
+            {
+                labelName: 'Número de emergência',
+                inputId: 'emergencyNumber'
+            },
+            {
+                labelName: 'Lista de autoriados a buscar a criança',
+                inputId: 'authorizationList'
+            },
+            {
+                labelName: 'Turma',
+                inputId: 'class'
+            },
+            {
+                labelName: 'Observações adicionais',
+                inputId: 'addInfos'
+            }
+        ];
+
+        return(
+            <>  
+                
+                <Header />
+                <FormRegister infos={formNewStudent}/>
+                <button onClick={Editpage}>CLIQUE AQUI</button>
+                <h1>PAGINA DE HOME</h1>
+            </>
+        );
+    }
+}
+
+export default Registerpage;
