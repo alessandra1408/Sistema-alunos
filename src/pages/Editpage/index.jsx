509abf4209bc610f1de678ced74a4681ae9e2d5e
alessandra1408 <alessandraoitnb@gmail.com>
2021-08-25 19:19:21 -0300

    Cria a pagina de edição de aluno

---
+++ src/pages/Editpage/index.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Header from '../../components/Header';
+import FormEdit from '../../components/FormEdit'
+
+
+class Editpage extends Component{
+    render(){
+        return(
+            <>
+            <Header />
+            <FormEdit />
+            </>
+        )
+    }
+}
+
+export default Editpage;
