6bbb76515aa2d3aaae228d382775b3fe0ae2c098
alessandra1408 <alessandraoitnb@gmail.com>
2021-08-25 19:42:39 -0300

    Inicializa routes

---
+++ src/route.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import {BrowserRouter, Switch, Route, Link
+} from "react-router-dom";
+import Editpage from "./pages/Editpage";
+import Homepage from "./pages/Homepage";
+import Registerpage from "./pages/Registerpage";
+import FormRegister from "./components/FormRegister";
+
+
+export default function Routes() {
+  return (
+    <BrowserRouter>
+
+        <Switch>
+
+          <Route exact path="/">
+            <Homepage />
+          </Route>
+
+          <Route path="/register" component={FormRegister}/>
+
+          <Route path="/editpage" component={Editpage}/>
+
+          {/* <Route>404 Not Found</Route> */}
+
+        </Switch>
+    </BrowserRouter>
+  );
+}
